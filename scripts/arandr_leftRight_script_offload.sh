55850a986e2be55973254327f9496e7bb36260cd
Menezess42 <arielmenezess@hotmail.com>
2023-12-29 16:12:34 -0300

    evolved

--- scripts/arandr_leftRight_script_offload.sh
+++ scripts/arandr_leftRight_script_offload.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
-
 NVIDIA_GPU_NUMBER=1
 
 export __NV_PRIME_RENDER_OFFLOAD=$NVIDIA_GPU_NUMBER
 
 if xrandr | grep "HDMI-1-0 connected"; then
-xrandr --output eDP-1   --mode 1920x1080 --pos 0x1082 --rotate normal --output HDMI-1-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal
-
+	xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal \
+		--output HDMI-1-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
 else
 	echo "HDMI não encontrado."
 fi
 
 unset __NV_PRIME_RENDER_OFFLOAD
-
--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-DISPLAY=":0" picom -b #-experimental-backends
+#DISPLAY=":0" picom -b #-experimental-backends
 # pasystray &
 flameshot &
+discord &
+spotify &
 #DISPLAY=":1" picom -b
 
